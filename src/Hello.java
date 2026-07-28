ba85e57ecc8cd367c8b3a6b3aeeef8561283c135
Kalavathi <kalavathidevulapalli274@gmail.com>
2026-07-28 21:42:04 +0530

    Updated output

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Jenkins CI/CD Demo");
+        System.out.println("Continuous Integration Successful");
 
     }
 
